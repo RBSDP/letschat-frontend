a4e014c742594f4675980c27afd8e29a629031e5
siva <raavibalasivadurgaprasad@gmail.com>
2026-04-19 01:03:05 +0530

    chore(setup): initialize React + TypeScript project with Vite and Tailwind CSS, add base folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-green-500">
+        Setup Clean & Working ✅
+      </h1>
+    </div>
+  );
+}
+export default App
